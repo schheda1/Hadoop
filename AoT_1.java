7b8e146331dae9d6278049f68fcb8c387a287a9e
smeetdc <55862771+smeetdc@users.noreply.github.com>
2020-11-14 16:57:49 -0500

    Update AoT_1.java

--- AoT_1.java
+++ AoT_1.java
@@ -7,12 +7,15 @@ import java.util.Date;
 import org.json.*;
 
 import org.apache.commons.lang.WordUtils;
+import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.input.*;
 import org.apache.hadoop.mapreduce.lib.output.*;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
 public class AoT_1 extends Configured implements Tool {
 	

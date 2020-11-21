eb6af96f76b73560d79f3e57a5d924a9fa6d26fa
smeetdc <55862771+smeetdc@users.noreply.github.com>
2020-11-20 20:18:42 -0500

    Update test.java

--- test.java
+++ test.java
@@ -24,7 +24,7 @@ public class Test {
                     Map.Entry pair = itr1.next(); 
                     System.out.println(pair.getKey() + " : " + pair.getValue()); 
                 } 
-                double latitiude = (double) jo.get("latitude");
+                double latitude = (double) jo.get("latitude");
                 System.out.print(latitude);
                 i++;
                 line = br.readLine();

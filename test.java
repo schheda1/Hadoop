3f6d8acccf3bca5e2bec782298c3eb2027c562b6
smeetdc <55862771+smeetdc@users.noreply.github.com>
2020-11-20 19:27:10 -0500

    Update test.java

--- test.java
+++ test.java
@@ -12,7 +12,7 @@ public class Test {
            while(i<3){
                 String data = sc.nextLine();
                 JSONObject obj1 = new JSONObject(data);
-                String features = obj1.getString("features");
+                String features = obj1.getString("latitude");
                 System.out.println(features.toString());
                 i++;
            }

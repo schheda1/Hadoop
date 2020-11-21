127c308d16c0b92d15d5f14591fa8c002d592766
smeetdc <55862771+smeetdc@users.noreply.github.com>
2020-11-20 19:10:00 -0500

    Update test.java

--- test.java
+++ test.java
@@ -1,7 +1,25 @@
 import org.json.JSONObject;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Test {
     public static void main(String args[]){
-       JSONObject jo = new JSONObject("{ \"abc\" : \"def\" }");
-       System.out.println(jo.toString());
+       try{
+           File obj = new File("aot-small-10k.json");
+           Scanner sc = new Scanner(obj);
+           int i=1;
+           while(i<3){
+                String data = sc.nextLine()
+                JSONObject obj1 = new JSONObject(data);
+                features = obj1.getString("features");
+                System.out.println(features.toString());
+                i++;
+           }
+           JSONObject jo = new JSONObject("{ \"abc\" : \"def\" }");
+           System.out.println(jo.toString());
+       } catch (FileNotFoundException fe) {
+           fe.printStackTrace();
+       }
     }
 }

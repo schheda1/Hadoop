7555458a0f6571fabc5571ec0b6cc0a62b442ab5
smeetdc <55862771+smeetdc@users.noreply.github.com>
2020-11-20 19:18:44 -0500

    Create test2.java

---
+++ test2.java
@@ -0,0 +1,7 @@
+import org.json.JSONObject;
+public class test2 {
+    public static void main(String args[]){
+       JSONObject jo = new JSONObject("{ \"abc\" : \"def\" }");
+       System.out.println(jo.toString());
+    }
+}

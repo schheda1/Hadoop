c39f6e1d41ea4bd803aafd39a7ebbb27272effb4
smeetdc <55862771+smeetdc@users.noreply.github.com>
2020-11-22 12:17:16 -0500

    Update AoT_2.java --final changes

--- AoT_2.java
+++ AoT_2.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.util.*;
 import java.text.SimpleDateFormat;
+import java.test.DateFormat;
 import java.util.Date;
 import java.util.Iterator; 
 import java.util.Map; 
@@ -21,43 +22,59 @@ import org.apache.hadoop.mapreduce.lib.input.*;
 import org.apache.hadoop.mapreduce.lib.output.*;
 
 public class AoT_2 {
-	public static String param;
-	public static String aggregator;
-	public static Date start_date;
-	public static Date end_date;	
+	//public static String param;
+	//public static String aggregator;
+	//public static Date start_date;
+	//public static Date end_date;	
 	
 	
 	// 4 types declared: Type of input key, type of input value, type of output key, type of output value
 	public static class MyMapper extends Mapper<Object, Text, Text, DoubleWritable> {
-		//private final static LongWritable one = new LongWritable(1);
 		
 		// The 4 types declared here should match the types that was declared on the top
 		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 			//String parameter;
 			//String default_parameter = "parameter";
+			Configuration cd = context.getConfiguration();
+			String param_arg = cf.get("param");
+			String st_date_arg = cf.get("start_date");
+			String ed_date-arg = cf.get("end_date");
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+			
+			String default_parameter = "parameter";
 			String line = value.toString();
             		String[] tuple = line.split("\\n");
 			double value2;
-			//double store_data[] = new store_data[tuple.length]; 
-			//int data_idx=0;
+			String val_hrf = "value_hrf";
+			
 			try{
+				Date start_date_arg = sdf.parse(st_date_arg);
+				Date end_date_arg = sdf.parse(ed_start_arg);
 				for(int i=0; i<tuple.length; i++){
 					Object obj2=JSONValue.parse(tuple[i]);
 					JSONObject jo = (JSONObject) obj2;
-
+					String key_temp = "";
+					int temp=0;
 					Map<String, String> features = ((Map<String,String>)jo.get("features"));
+					String t_val = String.valueOf(jo.get("timestamp"));
+					Date time_val = new Date(Long.parseLong(t_val));
 					for (Map.Entry<String, String> entry : features.entrySet()) {
 						String k = entry.getKey();
         					String v = entry.getValue();
 						long time_val = Long.parseLong(features.get("timestamp"));
-					    if (k.equals(param)) {
+					    	if (k.equals(default_parameter) && v.equals(param_arg) && time_val.after(start_date_arg) && time_val.before(end_date_arg)) {
 					    	//context.write(new Text(v), one);
-							value2 = Double.parseDouble(features.get("value_hrf"));
-							//store_data[data_idx] = Double.parseDouble(value);
-							//data_idx++;
-							context.write(new Text(v), new DoubleWritable(value2));
+							key_temp = v;
+							temp = 1;
+							//context.write(new Text(v), new DoubleWritable(value2));
 							
-					    }
+					    	}
+						if (k.equals(val_hrf) && temp==1){
+							value2 = Double.parseDouble(v);
+							temp=0;
+							conext.write(new text(key_temp), new DoubleWriteable(value2));
+						}
+						
 					}
 				}
 			} catch (Exception e){
@@ -78,37 +95,40 @@ public class AoT_2 {
 		// Notice the that 2nd argument: type of the input value is an Iterable collection of objects 
 		//  with the same type declared above/as the type of output value from map
 		public void reduce(Text key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException {
+			Configuration cf2 = context.getConfiguration();
+			String aggregator = cf2.get("aggregator");
+			
 			double sum = 0;
 			int counter = 0;
 			double max_val = Double.MIN_VALUE;
 			double min_val = Double.MAX_VALUE;
 			
-			if(aggregator.equals("max")){
+			if(aggregator == "max"){
 				for (DoubleWritable tmp: values) {
-					//sum += tmp.get();
-					//counter++;
 					if(tmp.get() > max_val){
 						max_val = tmp.get();
 					}
 				}
 				total.set(max_val);
 			}
-			else if(aggregator.equals("min")){
+			else if(aggregator == "min"){
 				for (DoubleWritable tmp: values) {
-					//sum += tmp.get();
-					//counter++;
 					if(tmp.get() < min_val){
 						min_val = tmp.get();
 					}
 				}
 				total.set(min_val);				
 			}
-			else {
+			else if(aggregator == "av"){
 				for (DoubleWritable tmp: values) {
 					sum += tmp.get();
 					counter++;
 				}
-				total.set(sum/counter);
+				try{
+					total.set(sum/counter);
+				} catch(NullPointerException ne) {
+					ne.printStackTrace();
+				}
 			}
 
 			// This write to the final output
@@ -118,19 +138,27 @@ public class AoT_2 {
 	
 	
 	public static void main(String[] args)  throws Exception {
-		start_date = new SimpleDateFormat("yyyy-mm-dd").parse(args[1]);
+		Date start_date_ = new SimpleDateFormat("yyyy-mm-dd").parse(args[1]);
 		//start_date = start_date.getTime();
-		end_date = new SimpleDateFormat("yyyy-mm-dd").parse(args[2]);
+		Date end_date_ = new SimpleDateFormat("yyyy-mm-dd").parse(args[2]);
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+		String st_date = df.format(start_date_);
+		String ed_date = df.format(end_date_);
 		//end_date = end_date.getTime();
-		param = args[3];
-		aggregator = args[4];
+		//param = args[3];
+		//aggregator = args[4];
 		Configuration conf = new Configuration();
+		conf.set("start_date", st_date);
+		conf.set("end_date", ed_date);
+		conf.set("param", args[3]);
+		conf.set("aggregator", args[4]);
+		
 		Job myjob = Job.getInstance(conf, "my specific parameter specific aggregate test");
-		myjob.setJarByClass(AoT_1.class);
+		myjob.setJarByClass(AoT_2.class);
 		myjob.setMapperClass(MyMapper.class);
 		myjob.setReducerClass(MyReducer.class);
 		myjob.setOutputKeyClass(Text.class);
-		myjob.setOutputValueClass(LongWritable.class);
+		myjob.setOutputValueClass(DoubleWritable.class);
 		// Uncomment to set the number of reduce tasks
 		// myjob.setNumReduceTasks(2);
 		FileInputFormat.addInputPath(myjob, new Path(args[0]));
